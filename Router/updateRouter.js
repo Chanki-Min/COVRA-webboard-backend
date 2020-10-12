b7d04c1bc43f66813a5c8e8604641281a2d66f6f
GreenApple <alscksrl2010@gmail.com>
2020-10-13 01:52:20 +0900

    Implement data update feature & make requirements.txt for python script
    파이썬 스크립트를 위한 의존성 설치 스크립트 추가
    데이터 업데이트 기능 추가

--- Router/updateRouter.js
+++ Router/updateRouter.js
@@ -6,16 +6,16 @@ const util = require('util');
 const stream = require('stream');
 const pipeline = util.promisify(stream.pipeline);
 const wrapAsyncFn = require('../Utils/wrapAsyncFn');
+const exec = require('child_process').exec;
+const execPromise = util.promisify(exec);
 const checkData = require('../Utils/checkData');
 
 router.post(
     "/updateData",
     wrapAsyncFn(async (req, res) => {
-        console.log('got updateData req');
-        const tmpFileName = process.env.DATA_FILE_NAME + process.env.DATA_FILE_EXT + '.tmp';
-
-        const downloadPath = path.join(process.env.DATA_DIR_PATH, tmpFileName);
-        const fileWriteStream = fs.createWriteStream(downloadPath);
+        const predictionPath = path.resolve(process.env.PATH_PYTHON_DIR, process.env.PATH_DOWNLOAD_PREDICTION);
+        const tmpPath = path.join(predictionPath) + '.tmp';
+        const fileWriteStream = fs.createWriteStream(tmpPath);
 
         //save tmp data
         await pipeline(
@@ -23,15 +23,24 @@ router.post(
             fileWriteStream
         );
 
+        //move tmp file
+        await fs.promises.rename(tmpPath, predictionPath);
+
         //파이썬 돌리기
+        const scriptPath = path.resolve(process.env.PATH_PYTHON_DIR, 'main.py');
+        await execPromise(`python ${scriptPath}`);
 
-
-        //data check
-
+        //TODO : data check
 
         //데이터  파일 교체
+        const processFilePath = path.resolve(process.env.PATH_PYTHON_DIR, process.env.PATH_DATA_JSON);
+        const targetPath = path.resolve(process.env.PATH_DATA);
+        await fs.promises.rename(processFilePath, targetPath);
 
         //데이터 global에 리로딩
+        let data = await fs.promises.readFile(process.env.PATH_DATA);
+        data = JSON.parse(data.toString());
+        global.data = data;
 
         res.set(200).send({result: 'ok'});
     })
